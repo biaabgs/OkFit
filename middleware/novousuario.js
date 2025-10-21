364b96145e97b9f248d65bfb76e2b2157e847028
Bia <goncalvesdasilvabia@gmail.com>
2025-10-21 17:34:24 -0300

    atualizacoes dia 21 de outubro

--- middleware/novousuario.js
+++ middleware/novousuario.js
@@ -6,49 +6,50 @@ const path = require('path')
 //caminho das informações dos usuários
 const dataPath = path.join(__dirname, '..', 'data', 'users.json')
 
-router.post('/planos/assinatura', async (req , res) => {
-    try{
-      
+router.post('/planos/assinatura', async (req, res) => {
+    try {
+
         //pede as informações do novo usuário
-        const {nome, email, senha, plano ,telefone ,datanasc}=req.body
-        
+        const { nome, email, senha, plano, telefone, datanasc } = req.body
+
         //verifica se todos os campos do formulário estão preenchidos
-        if(!nome || !email || !senha || !plano || !telefone || !datanasc ){
-            return res.status(400).json({error: "Preencha todos os campos!"})
+        if (!nome || !email || !senha || !plano || !telefone || !datanasc) {
+            return res.status(400).json({ error: "Preencha todos os campos!" })
         }
 
         //lê o arquivo dos users (users.json)
         const data = await fs.readFile(dataPath, 'utf8')
         //transforma a informação em json para que possamos manusear a informação
-		const users = JSON.parse(data)
+        const users = JSON.parse(data)
         //procura com um email igual ao que está no formulário
         const emailExistente = users.find(user => user.email === email)
 
         //Verifica se há um email igual no sistema
         //se a sentença for verdade(haver um email igual a do sistema), o sistema manda uma mensagem
         if (emailExistente) {
-			return res.status(400).json({ error: "Email já cadastrado!" })
-		}
+            return res.status(400).json({ error: "Email já cadastrado!" })
+        }
 
         //Segue colocando as informações do formulário no formaro de configuração do JSON
-        let userID = Date.now()
-        
-        const novoUsuario = {
-			email: email,
-			passwd: passwd,
-			tipoConta: tipoConta,
-			userID: userID,
-			dados: {}
-		};
-        //Coloca o novo usuário no arquivo users.json
-        users.push(novoUsuario);
-        await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+        let ID = Date.now()
 
-		res.status(201).json({ success: true }) // Envia mensagem de sucesso
-    } //Erro 
+        const novoUsuario = {
+            nome: nome,
+            email: email,
+            senha: senha,
+            plano: plano,
+            telefone: telefone,
+            datanasc: datanasc
+        }
+    };
+    //Coloca o novo usuário no arquivo users.json
+    users.push(novoUsuario);
+    await fs.writeFile(dataPath, JSON.stringify(users, null, 2));
+
+    res.status(201).json({ success: true }) // Envia mensagem de sucesso
+} //Erro 
     catch (error) {
-		res.status(500).json({ error })
-	}
+    res.status(500).json({ error })
 })
 // Exporta o router
 module.exports = router
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -15,7 +15,7 @@ function registrarUsuario() {
 
     //criação das constantes de input e radio(tipo de plano)
     const email = document.getElementById('emailNewUser').value
-	const passwd = document.getElementById('passwdNewUser').value
+	const senha = document.getElementById('passwdNewUser').value
 	const presencial = document.getElementById('presencial').checked
 	const hibrido = document.getElementById('hibrido').checked
     const online = document.getElementById('online').checked
@@ -33,4 +33,103 @@ function registrarUsuario() {
     if (online) {
 		plano = 'online'
 	}
+
+	//verifica se os campos estão selecionados
+	if(!email || !senha || !plano){
+		alert('Por favor preencha todos os campos!')
+		return
+	}
+
+	// Verifica regras de senha
+	const senhaForte = const senhaComEspecial = /^(?=.*[\W_]).{8,}$/
+
+	
+	if (!senhaForte.test(senha)) {
+		alert('[Senha fraca] Sua senha precisa ter pelo menos 8 caracteres, incluindo letras maiúsculas, minúsculas, números e caracteres especiais.');
+		return;
+	}
+
+	fetch('/api/registrar', {
+		// Parametros que seriam inseridos no 'curl' já são inseridos aqui
+		method: 'POST', // -X POST
+		headers: { // -H "Content-type: application/json"
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({email, senha, plano }) // -d (Aqui é pego os dados inseridos nos inputs e transformados em um body para requisição)
+	})
+	.then(res => res.json())
+	.then(data => {
+
+		// Constante para pegar valor informado no Modal do Bootstrap
+		const modal = bootstrap.Modal.getInstance(
+                document.getElementById('exampleModal')
+            );
+
+        modal.hide();
+            
+        document.getElementById('emailNewUser').value = ''
+        document.getElementById('passwdNewUser').value = ''
+
+		if (data.success) {
+			alert('Usuário registrado com sucesso!');
+		} 
+		//Tratamento de erro em registrar o usuário
+		else {
+			alert('Erro ao registrar usuário: ' + (data.error || 'erro desconhecido'))
+		}
+	})
+
+	// erro
+	.catch(err => {
+		console.error(err)
+		alert('Erro no servidor')
+	});
+
 }
+
+function fazerlogin() {
+
+	//input do login
+	const email = document.getElementById('email').value
+	const senha = document.getElementById('senha').value
+
+	//verificação se os nenhum campo está vazio
+	if(!email || !senha){
+		alert('Por favor preencha todos os campos!')
+		return
+	}
+
+	fetch('/api/login', {
+		// curl
+		headers: { // -H "Content-type: application/json"
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({email, senha}) 
+	.then(res => res.json())
+	.then(data => {
+
+		if (data.success) {
+			// recebe o userID do user.json
+			const ID = data.usuario.ID;
+
+			// Variaveis são armazenadas localmente, para serem vistos pelo JSON
+			localStorage.setItem('ID', ID);
+			localStorage.setItem('userEmail', data.usuario.email);
+			localStorage.setItem('userTipo', data.usuario.plano);
+
+			// Redireciona o aluno para a página do seu plano
+			window.location.href = `/catalogo/${plano}`;
+			alert('Usuario logado com sucesso!');
+		} 
+		//Tratamento de erro em registrar o usuário
+		else {
+			alert('Erro ao registrar usuário: ' + (data.error || 'erro desconhecido'))
+		}
+	})
+
+	// erro
+	.catch(err => {
+		console.error(err)
+		alert('Erro no servidor')
+	});
+})}
