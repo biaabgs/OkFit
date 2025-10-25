1a9b8030df6906eba74957a8b4b9ac9783e352fc
Bia <goncalvesdasilvabia@gmail.com>
2025-10-25 14:49:52 -0300

    Atualiza estrutura e conteúdo dos treinos, adicionando novos exercícios e ajustando a exibição no frontend

--- public/assets/js/treinos.js
+++ public/assets/js/treinos.js
@@ -14,9 +14,58 @@ function deleteTask(button) {
   setTimeout(() => colDiv.remove(), 300);
 }
 
-document.getElementById("taskInput").addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    addTask();
-  }
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("assets/js/treinos.json")
+    .then(res => res.json())
+    .then(data => {
+      data.treinos.forEach(treino => {
+        const id = treino.id;
+
+        const img = document.getElementById(`img-${id}`);
+        const titulo = document.getElementById(`titulo-${id}`);
+        const desc = document.getElementById(`desc-${id}`);
+        const card = document.getElementById(`card-${id}`);
+
+        if (img) img.src = treino.imagem;
+        if (titulo) titulo.textContent = treino.titulo;
+        if (desc) desc.textContent = treino.descricao;
+        if (card) {
+          card.onclick = () => {
+            window.location.href = `treino.html?id=${id}&nome=${treino.nomeurl}`;
+          };
+        }
+      });
+    })
+    .catch(error => console.error("Erro ao carregar treinos.json:", error));
 });
+
+document.addEventListener("DOMContentLoaded", () => { /* DOMContentLoaded --> espera o HTML ser executado para executar o JS */
+  const params = new URLSearchParams(window.location.search); /* window.location.search --> recebe tudo que vem depois do ? (no caso ?id=${id}) */
+  /* esse params relaciona o html especifico (treino.html) com o generalizado (treinos.html) pois recebe do URL o id */
+  const treinoId = parseInt(params.get("id"));
+  if (!treinoId) return;
+
+  fetch("assets/js/treinos.json")
+    .then(res => res.json())
+    .then(data => {
+      const treino = data.treinos.find(t => t.id === treinoId);
+      if (!treino) return;
+
+      const tituloElement = document.querySelector(".main-titulo");
+      if (tituloElement) tituloElement.textContent = treino.titulo;
+      treino.exercicios.forEach((ex, index) => {
+        const nomeElemento = document.getElementById(`ex-nome-${index + 1}`); /* index + pois o index comeca em 0 */
+        const descElemento = document.getElementById(`ex-desc-${index + 1}`);
+
+        if (nomeElemento) nomeElemento.textContent = ex.nome;
+        if (descElemento) descElemento.innerHTML = ex.descricao;
+      });
+    })
+    .catch(err => console.error("Erro ao carregar treino:", err));
+});
+
+/* Titulo --> nome do TREINO (exemplo: Peito e Tríceps)
+Nome --> nome do EXERCICIO (exemplo: )*/
+
+
 
