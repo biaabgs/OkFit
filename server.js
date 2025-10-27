f86fa6ff12dd6d41f72d76265dbd0167b5b6970d
biaabgs <goncalvesdasilvabia@gmail.com>
2025-10-27 13:26:56 -0300

    penultimo commit biblioteca

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const path = require('path')
 const app = express()
 const port = 3000
 
-const index = require('./middleware/back')
+// const index = require('./middleware/back')
 const login = require('./middleware/loginUser')
 const registrarRoute = require('./middleware/registrarUser')
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.use('/api/info', index);
+// app.use('/api/info', index);
 app.use('/api/login', login);
 app.use('/api/registrar', registrarRoute);
 app.use('/api/catalogo', catalogo);
